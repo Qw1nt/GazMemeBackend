152efb918a56c7e99d801063a50d835aed782fe1
WhCollar <v.zadvornyj@iqdev.digital>
2023-06-28 03:21:41 +0500

    Test

--- Application/Messages/Direction/Commands/CreateDirectionCommand.cs
+++ Application/Messages/Direction/Commands/CreateDirectionCommand.cs
@@ -3,4 +3,4 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Messages.Direction.Commands;
 
-public record CreateDirectionCommand(string Title, string Description, int EmployeeId, [FromForm] IFormFile Preview, [FromForm] IFormFileCollection Images);
+public record CreateDirectionCommand(string Title, string Description, int EmployeeId, [FromForm] IFormFile Preview, [FromForm] ICollection<IFormFile> Images);
