f39c76e9064c1adcd16c73cdd394ec4c06af5bac
WhCollar <v.zadvornyj@iqdev.digital>
2023-06-27 18:58:45 +0500

    Test

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,4 @@
 global using FastEndpoints;
-global using FastEndpoints.Security;
 using Application;
 using FastEndpoints.Swagger;
 using Infrastructure;
@@ -8,21 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-services.AddSingleton(builder.Environment);
-services.AddFastEndpoints();
-
 services.AddApplicationServices(configuration);
 services.AddInfrastructureServices(configuration);
-services.SwaggerDocument();
+
 services.AddCors(p => p.AddPolicy("CORSApp", corsPolicyBuilder =>
 {
     corsPolicyBuilder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
+services.AddSingleton(builder.Environment);
+services.AddFastEndpoints();
+services.SwaggerDocument();
+
+
 var app = builder.Build();
 
-
-app.UseHttpsRedirection();
 
 app.UseCors("CORSApp");
 app.UseStaticFiles();
