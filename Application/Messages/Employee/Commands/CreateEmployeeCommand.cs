8244c7f86b2fcd691a77844d5677bf97820fc7ba
WhCollar <monserka@gmail.com>
2023-06-29 17:30:00 +0500

    Stupid wish

--- Application/Messages/Employee/Commands/CreateEmployeeCommand.cs
+++ Application/Messages/Employee/Commands/CreateEmployeeCommand.cs
@@ -3,4 +3,4 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Messages.Employee.Commands;
 
-public record CreateEmployeeCommand(string FirstName, string LastName, string Surname, [FromForm] IFormFile Image, string Phone, string Email);
+public record CreateEmployeeCommand(string LastName, string FirstName, string Surname, [FromForm] IFormFile Image, string Phone, string Email);
